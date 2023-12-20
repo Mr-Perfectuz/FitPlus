1c82a233bebbc3551a13cf80dfe9eafc970bc10d
Mr-Perfectuz <ziyodovanvar1999@example.com>
2023-12-20 18:23:55 +0900

    feat: develop user.model schema

--- schema/user.model.js
+++ schema/user.model.js
@@ -1,55 +1,96 @@
 const mongoose = require("mongoose");
-const { member_type_enums, member_type_status } = require("../lib/config");
+const {
+  member_type_enums,
+  member_type_status,
+  ordinary_enums,
+} = require("../lib/config");
 
-const userSchema = new mongoose.Schema({
-  user_nick: {
-    type: String,
-    required: true,
-    index: { unique: true, sparse: true },
-  },
-  user_phone: {
-    type: String,
-    required: true,
-  },
-  user_password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  user_type: {
-    type: String,
-    required: false,
-    default: "USER",
-    enum: {
-      values: member_type_enums,
-      message: "{VALUE} is not among permitted values",
+const userSchema = new mongoose.Schema(
+  {
+    user_nick: {
+      type: String,
+      required: true,
+      index: { unique: true, sparse: true },
+    },
+    user_phone: {
+      type: String,
+      required: true,
+    },
+    user_password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    user_type: {
+      type: String,
+      required: false,
+      default: "USER",
+      enum: {
+        values: member_type_enums,
+        message: "{VALUE} is not among permitted values",
+      },
+    },
+    user_status: {
+      type: String,
+      required: false,
+      default: "ACTIVE",
+      enum: {
+        values: ["BLOCKED", "ACTIVE", "DELETED"],
+        message: member_type_status,
+      },
+    },
+    user_description: {
+      type: String,
+      required: false,
+    },
+    user_adress: {
+      type: String,
+      required: false,
+    },
+    user_image: {
+      type: String,
+      required: false,
+    },
+    user_point: {
+      type: Number,
+      required: false,
+    },
+    user_top: {
+      type: String,
+      required: false,
+      default: "ACTIVE",
+      enum: {
+        values: ["BLOCKED", "ACTIVE", "DELETED"],
+        message: ordinary_enums,
+      },
+    },
+    user_views: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    user_comment: {
+      type: String,
+      required: false,
+      default: 0,
+    },
+    user_likes: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    user_follow_cnt: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    user_subscriber_cnt: {
+      type: Number,
+      required: false,
+      default: 0,
     },
   },
-  user_status: {
-    type: String,
-    required: false,
-    default: "ACTIVE",
-    enum: {
-      values: ["BLOCKED", "ACTIVE", "DELETED"],
-      message: member_type_status,
-    },
-  },
-  user_full_name: {
-    type: String,
-    required: false,
-  },
-  user_adress: {
-    type: String,
-    required: false,
-  },
-  user_image: {
-    type: String,
-    required: false,
-  },
-  user_point: {
-    type: Number,
-    required: false,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
